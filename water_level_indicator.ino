2d101d10ea2cd06ed0bc41440145e1b28e6e1862
Abhishek20593 <59052276+Abhishek20593@users.noreply.github.com>
2019-12-20 22:48:13 +0530

    First Commit

--- water_level_indicator.ino
+++ water_level_indicator.ino
@@ -2,7 +2,6 @@
   #include <LiquidCrystal.h>
   LiquidCrystal lcd(8,9,4,5,6,7);
   byte sensorPin[] = {2,3,4};
-  byte ledPin[] = {11, 12, 13}; // number of leds = numbers of sensors
   const byte sensors = 3; // sensor analog input from A2
   int level = 0; 
   int motor = A5;
@@ -10,7 +9,6 @@
   void setup() {
     for(int i = 0; i < sensors; i++) {
        pinMode(sensorPin[i], INPUT); 
-       pinMode(ledPin[i], OUTPUT);
     }
     pinMode(motor, OUTPUT);
     lcd.begin(16, 2);  
@@ -20,20 +18,18 @@
     level = 0; 
     for(int i = 0; i < sensors; i++) {
        if(digitalRead(sensorPin[i]) == LOW) {
-          digitalWrite(ledPin[i], HIGH);
           level = sensors - i;
-       } else {
-         digitalWrite(ledPin[i], LOW);       
        }    
     }
     lcd.clear();
     lcd.print("Water level");
     lcd.setCursor(0,1);  
     switch(level) {
-       case 1:    lcd.print("HIGH");      digitalWrite(motor, HIGH);  break;
-       case 2:    lcd.print("AVERAGE");   digitalWrite(motor, LOW);   break;
-       case 3:    lcd.print("LOW");       digitalWrite(motor, LOW);   break;
-       default:   lcd.print("NO WATER");  digitalWrite(motor, LOW);   break;
+       case 1:    lcd.print("HIGH");      digitalWrite(motor, LOW);  break;
+       case 2:    lcd.print("AVERAGE");   digitalWrite(motor, HIGH);   break;
+       case 3:    lcd.print("LOW");       digitalWrite(motor, HIGH);   break;
+       default:   lcd.print("NO WATER");  digitalWrite(motor, HIGH);   break;
     }
-    delay(50);  
+    delay(5000);  
    }
+   
